cf2110087b054cd45de7e6ee836e17bf46e1c96e
Miriam <miriamlunacarreon133@gmail.com>
2022-05-05 22:45:24 -0500

    requerimiento 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -11,4 +11,12 @@ describe("Unit test for User object", ()=>{
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
     })
+
+    test("Add getters", ()=>{
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        expect(user.getUsername()).toBe("carlogilmar")
+        expect(user.getBio()).toBe("Bio")
+        expect(user.getDateCreated()).not.toBeUndefined()
+        expect(user.getLastUpdated()).not.toBeUndefined()
+    })
 })
